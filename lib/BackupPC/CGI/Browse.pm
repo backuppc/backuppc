1261ba6d7a7ba0c973f25b8a63987360a639e16c
Alexander Moisseev <moiseev@mezonplus.ru>
2015-01-07 15:11:49 +0300

    Browse.pm: - Remove deprecated defined(@array) perl5160delta: http://perldoc.perl.org/perldiag.html#defined(@array)-is-deprecated

--- lib/BackupPC/CGI/Browse.pm
+++ lib/BackupPC/CGI/Browse.pm
@@ -65,7 +65,7 @@ sub action
     #
     # default to the newest backup
     #
-    if ( !defined($In{num}) && defined(@Backups) && @Backups > 0 ) {
+    if ( !defined($In{num}) && @Backups > 0 ) {
         $i = @Backups - 1;
         $num = $Backups[$i]{num};
     }

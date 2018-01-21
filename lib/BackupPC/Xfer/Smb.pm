5e70e006269a3ec73f96251cbbfc1552cb4d3c5d
Craig Barratt <craigb@gmail.com>
2018-01-20 17:18:01 -0800

    also ignore empty output lines from smbclient; fixes issue #159.

--- lib/BackupPC/Xfer/Smb.pm
+++ lib/BackupPC/Xfer/Smb.pm
@@ -28,7 +28,7 @@
 #
 #========================================================================
 #
-# Version 4.1.3, released 3 Jun 2017.
+# Version 4.1.4, released 24 Nov 2017.
 #
 # See http://backuppc.sourceforge.net.
 #
@@ -315,6 +315,7 @@ sub readOutput
                 || /^\s*creating lame (up|low)case table/i
                 || /^\s*rlimit_max: increasing rlimit_max/i
                 || /^\s*OS=\[/i
+                || /^\s*$/
 	    ) {
             # ignore these messages
             $t->{XferLOG}->write(\"$_\n") if ( $t->{logLevel} >= 1 );
